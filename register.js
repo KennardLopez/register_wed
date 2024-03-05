885c57ba8c88c578509c838b7fdc2b201fabee90
KennardLopez <kennardmamucodlopez@gmail.com>
2024-03-05 16:19:16 +0800

    register_web

---
+++ register.js
@@ -0,0 +1,26 @@
+console.log("Hello World")
+console.error("ERROR HAPPENED!!!")
+console.warn("WARNINGG!!")
+
+// Variables
+// string = "words" 
+// number = 123
+// boolean = true, false
+// object = {"apple, banana"} {"1,2,3,4"}
+
+
+let name = "Kennard"
+let lastname = "Lopez"
+let age = "18"
+let isProf = true
+hobbies = ["Eating","listening music"];
+run = function () {
+    console.log("im running.")
+};
+console.log(name, age, isProf, hobbies, run)
+console.log(typeof run);
+console.table(hobbies)
+console.log("My name is " + name + " " + lastname);
+console.log(`My name is ${name} ${lastname}`)
+
+
